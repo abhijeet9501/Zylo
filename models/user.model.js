5d59e8147f34085083055787ac1d5fd975222123
Abhijeet Swami <swami2005abhijeet@gmail.com>
2025-04-28 19:05:31 +0530

    add user model

---
+++ models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            lowercase: true,
+            trim: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            lowercase: true,
+            trim: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        avatar: {
+            type: String,
+        },
+        bio: {
+            type: String,
+        },
+    }, 
+    {timestamps: true}
+);
+
+userSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.checkPassword = async function(password) {
+    try {
+        return await bcrypt.compare(password, this.password);
+    }
+    catch {
+        return null;
+    }
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
