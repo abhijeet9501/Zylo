f16a73d3457cbd44a39165b6852e98bc0e5a53a9
Abhijeet Swami <swami2005abhijeet@gmail.com>
2025-04-29 09:51:40 +0530

    Add global error handling

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,7 +1,14 @@
 const errorMiddleware = (err, req, res, next) => {
-    const statusCode = err.statusCode;
+    const statusCode = err.statusCode || 500;
     const message = err.message || "Something went wrong!";
-    res.status(statusCode).json(message);
+
+    res.status(statusCode)
+    .json(
+        {
+            sucess: false,
+            message,
+        }
+    );
 };
 
-export default errorMiddleware;
+export { errorMiddleware };
