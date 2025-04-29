7c9f2f50d5a3e29cfc48d15aaffd3f898e03a519
Abhijeet Swami <swami2005abhijeet@gmail.com>
2025-04-29 09:56:03 +0530

    add error route

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
+import authRouter from "./routes/auth.route.js";
 
 
 const app = express();
@@ -8,10 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-
-app.get('/', (req, res) => {
-    res.send("Your server is running fine!");
-});
+app.use("/api/v1/auth", authRouter);
+app.use(errorMiddleware);
 
 export {
     app
