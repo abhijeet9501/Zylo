70495dd0f4c6915dce256a0a53afbb272b5826e2
Abhijeet Swami <swami2005abhijeet@gmail.com>
2025-05-07 09:48:07 +0530

    add search user

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,2 +1,18 @@
-
 renderUser(false);
+
+const searchUsername = document.getElementById("search-username");
+
+searchUsername.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      const value = searchUsername.value.trim();
+      
+      if (value == "") {return};
+      redirect(value); 
+    }
+});
+
+const redirect = (username) => {
+    const URL = `/user-profile.html?username=${username}`;
+
+    window.location.href = URL;
+};
